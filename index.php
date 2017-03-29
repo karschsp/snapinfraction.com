0101de0f5ea19c821d75c8062dc47f341bed07cc
Steve Karsch <steve@stevekarsch.com>
2017-03-28 23:26:55 -0400

    Initial commit of the Chin Music version of snapinfraction.com

---
+++ index.php
@@ -0,0 +1,392 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Snap Infraction - Chin Music
+    </title>
+    <meta name="description" content="Snap Infraction - Chin Music">
+    <meta name="author" content="Snap Infraction">
+    <!-- link rel="stylesheet" href="css/styles.css?v=1.0" -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+<![endif]-->
+    <style type="text/css">
+      @import url('https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700');
+	  @import url('https://fonts.googleapis.com/css?family=Sanchez');
+
+      body {
+        /* background-image: url('http://www.snapinfraction.com/singular_background.jpg');
+        background-repeat: no-repeat;
+        background-position-x: 50%; */
+        background: url('chin_music_background.jpg') 50% 0 no-repeat;
+        color: rgba(255,255,255,0.8);
+        background-color: #000;
+        text-align: left;
+        font-size: 2.7vmin;
+        line-height: 3.6vmin;
+        font-family: 'Lato', 'Helvetica Neue', Arial, sans-serif;
+        font-weight: 300;
+      }
+      h1, h2, h3, h4, h5, h6 {
+        font-family: "Sanchez", "Roboto Slab", "Consolas", "Courier", monospace, serif;
+        font-weight: 300;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        text-align: center;
+      }
+      h1, h2 {
+        text-indent: -9999px;
+      }
+      iframe {
+        text-align: center;
+      }
+      strong {
+        font-weight: 400;
+      }
+      #container {
+        /* display: flex;
+        display: -webkit-flex; */
+        background: url('/crowd.jpg') 0 80% no-repeat;
+        width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        font-size: 4.0vmin;
+        line-height: 5.6vmin;
+      }
+      /* ..content {
+      -webkit-flex: 1 0 0;
+      flex: 1 0 0;
+      display: block;
+      clear: both;
+      }  */    
+      a:link, a:visited, a:hover, a:active {
+        color: #fff;
+        text-transform: uppercase;
+        text-decoration: none;
+      }
+      a:hover, a:active {
+        color: orange;
+      }
+      h3 {
+        font-size: 36px;
+      }
+      #menu, #folow {
+        display: inline-block;
+        text-align: center;
+        margin-top: 200px;
+        width: 100%;
+        margin-left: 0;
+        padding-left: 0;
+      }
+      #buy {
+        display: inline-block;
+      }
+      #buy li {
+        display: inline;
+        margin-right: 8px;
+        letter-spacing: 1px;
+      }
+      #menu li, #follow li {
+        display: inline-block;
+        width: 22%;
+      }
+      #menu li a {
+        font-weight: 300;
+        letter-spacing: 1px;
+        font-size: 2vw;
+      }
+      #follow {
+        margin-top: 0;
+        width: 100%;
+        text-align: center;
+        padding-left: 0;
+      }
+      #follow li {
+        width: 25%;
+      }
+      #slide1, #slide2, #slide3, #slide4, #slide5, #slide6 {
+        color: #333;
+        /* padding: 170px 0 0 0; */
+        /* height: 600px; */
+        height:  50vh;
+        width: 100%;
+        background-size: cover;
+        /* background-color: rgba(255, 0, 0, 0.2); */
+	    -moz-box-shadow: inset 0px 20px 40px 0px #000000;
+    	-webkit-box-shadow: inset 0px 20px 40px 0px #000000;
+    	box-shadow: inset 0px 20px 40px 0px #000000;        
+      }
+      #slide1 {
+        background: url('/session-01.jpg') 50% 11% no-repeat fixed;
+      }
+      #slide2 {
+        background: url('/steve-les-paul.jpg') 50% 0 no-repeat fixed;
+      }
+      #slide3 {
+        background: url('/tony-drums.jpg') 50% 0 no-repeat fixed;
+      }
+      #slide4 {
+        background: url('/continental.jpg') 50% 0 no-repeat fixed;
+      }
+      #slide5 {
+        background: url('/ringo-drums.jpg') 50% 0 no-repeat fixed;
+      }
+      #slide6 {
+        background: url('/tony-mixing.jpg') 50% 0 no-repeat fixed;
+      }
+      .content {
+        width: 100%;
+        font-size: 16px;
+        line-height: 29px;
+      }
+      .content {
+        margin: 0 auto;
+        width: 50%;
+        text-align: left;
+        font-size: 18px!important;
+        line-height: 28px!important;
+        font-weight: normal;
+        text-align: left;
+      }
+      footer.content {
+        text-align: center;
+        margin-top: 24px;
+        margin-bottom: 24px;
+      }
+      
+      @media only screen and (min-device-width: 0px) and (max-device-width: 760px) {
+      #menu li a {
+        font-weight: 300;
+        letter-spacing: 1px;
+        font-size: 48px;
+      }      
+        .content {
+          width: 90%;
+          font-size: 36px!important;
+          line-height: 48px!important;
+        }
+        #video-player {
+          width: 100%;
+          height: 100%;
+        }
+		  #slide1 {
+			background: url('/session-01.jpg');
+		  }
+		  #slide2 {
+			background: url('/steve-les-paul.jpg');
+		  }
+		  #slide3 {
+			background: url('/tony-drums.jpg');
+		  }
+		  #slide4 {
+			background: url('/continental.jpg');
+		  }
+		  #slide5 {
+			background: url('/ringo-drums.jpg');
+		  }
+		  #slide6 {
+			background: url('/tony-mixing.jpg');
+		  }             
+		#follow li a img {
+		  width: 75px;
+		}        
+      }
+      @media only screen and (min-width: 0px) and (max-width: 760px) {
+        .content {
+          width: 90%;
+          font-size: 24px!important;
+          line-height: 36px!important;
+        }
+        #video-player {
+          width: 100%;
+          height: 100%;
+        }   
+		  #slide1 {
+			background: url('/session-01.jpg') 60% 5% no-repeat fixed;
+		  }
+		  #slide2 {
+			background: url('/steve-les-paul.jpg') 50% 25% no-repeat fixed;
+		  }
+		  #slide3 {
+			background: url('/tony-drums.jpg') 50% 25% no-repeat fixed;
+		  }
+		  #slide4 {
+			background: url('/continental.jpg') 50% 25% no-repeat fixed;
+		  }
+		  #slide5 {
+			background: url('/ringo-drums.jpg') 50% 25% no-repeat fixed;
+		  }
+		  #slide6 {
+			background: url('/tony-mixing.jpg') 50% 25% no-repeat fixed;
+		  }             
+		#follow li a img {
+		  width: 75px;
+		}
+      }      
+    </style>
+  </head>
+  <body>
+    <div id="container">
+      <div class="content">
+        <h1>Snap Infraction
+        </h1>
+        <h2>Chin Music
+        </h2>
+        <ul id="menu">
+          <li>
+            <a href="#music">Music
+            </a>
+          </li>
+          <li>
+            <a href="#video">Video
+            </a>
+          </li>
+          <li>
+            <a href="#bio">Bio
+            </a>
+          </li>
+          <li>
+            <a href="#follow">Follow
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div id="slide1">
+      </div>
+      <div class="content">
+        <a name="music">
+        </a>
+        <h3>Music
+        </h3>
+        <h4>Chin Music
+        </h4>
+        <iframe src="https://embed.spotify.com/?uri=spotify%3Aalbum%3A1yYX10WZXRXc56kawEORiP" width="100%" height="380" frameborder="0" allowtransparency="true">
+        </iframe>
+        <h5>Buy:
+        </h5>
+        <ul class="links" id="buy">
+          <li>
+            <a href="">iTunes
+            </a>
+          </li>
+          <li>
+            <a href="">Amazon
+            </a>
+          </li>
+          <li>
+            <a href="">CDBaby
+            </a>
+          </li>
+          <li>
+            <a href="">Google Music
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div id="slide2">
+      </div>
+      <div class="content">
+        <h4>Credits
+        </h4>
+        <p>
+          <strong>Snap Infraction is:
+          </strong>
+          <br />
+          <strong>Tony Iannuzzi
+          </strong> - Drums
+          <br />
+          <strong>Steve Karsch
+          </strong> - Everything but drums
+        </p>
+        <p>Produced by Snap Infraction
+        </p>
+        <P>Engineered and mixed by Tony Iannuzzi
+          <br /> "The Historian" and "Window Seat" mixed by 
+          <a href="http://www.cliffhillis.com">Cliff Hillis
+          </a>
+          </p>
+        <p>Recorded in Cinnaminson, NJ, Riverton, NJ, Aston, PA, Denver, CO.
+        </p>
+        <p>Mastered by 
+          <a href="http://www.cliffhillis.com">Cliff Hillis
+          </a> at The Hacienda in Phoenixville, PA.
+        </p>
+        <p>Cover photo by Steve Karsch
+        </p>
+        <p>Songs written by Steve Karsch (&copy; 2017 157 Music 
+          <a href="http://www.bmi.com">BMI
+          </a>)
+        </p>
+        <p>Thanks for listening.
+        </p>
+      </div>
+      <div id="slide3">
+      </div>
+      <div class="content">
+        <a name="video">
+        </a>
+        <h3>Video
+        </h3>
+        <iframe id="video-player" src="https://player.vimeo.com/video/20545285?title=0&byline=0&portrait=0" width="640" height="320" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+        </iframe>
+        <p>&nbsp;</p>
+      </div>
+      <div id="slide4">
+      </div>
+      <div class="content">
+        <a name="bio">
+        </a>
+        <h3>Bio
+        </h3>
+        <p>
+          <strong>Chin Music
+          </strong>, the debut EP from Philadelphia-area duo Snap Infraction, is a tight collection of 5 original hook-heavy songs drawing on the band's wide array of influences. Written over a span of ten years, and recorded mostly at home in the span of ten months, the EP is a straight-forward rock record with a nod toward the classics: Beatles, Stones and Zeppelin, without relying heavily on any particular influence.
+        </p>
+        <p>Songwriter and multi-instrumentalist Steve Karsch explains: "At the end of the day, what we really care about is the song. It may have a country flavor to it, a heavy blues rock vibe, or a sickenly-sweet pop melody; if it's a good song, if it's a song we both like, it's a song for Snap."
+        </p>
+        <p>After years of playing together in bands up and down the east coast, Steve and drummer/audio engineer Tony Iannuzzi grew tired of the same old band politics, and vowed to start a project on their own terms, they just didn't think it would take this long.
+        </p>
+        <p>"The plan for Snap Infraction has always been that there is no plan. We may never release another record, or we could start recording a new album tomorrow...or we could wait another decade. You never know."
+        </p>
+        <p>After writing and demoing up to 50 songs, the pair decided it was finally time to buckle down and introduce the world to Snap Infraction. Not willing to compromise on their vision, the duo decided to take on all aspects of production, from writing and performing to engineering and mixing. When it came time to put the finishing touches on the record, the pair called in power-pop legend Cliff Hillis to help with final mixes and mastering.
+        </p>
+        <p>"We've always been fans of the classics," Karsch continues. "And of the guitar-bass-drums combination. No frills, nothing fancy, just good, solid rock n roll."
+        </p>
+        <p>The result is Chin Music, a short trip through the past half-century of rock n roll with a look towards what may lie ahead.
+        </p>
+      </div>
+      <div id="slide5">
+      </div>
+      <div class="content">
+        <a name="follow">
+        </a>
+        <h3>Follow
+        </h3>
+        <ul id="follow" class="links">
+          <li>
+            <a href="https://twitter.com/snapinfraction"><img src="twitter-icon.png" alt="Follow @snapinfraction on Twitter" /></a>
+          </li>
+          <li>
+            <a href="https://facebook.com/snapinfraction"><img src="fb-icon.png" alt="Follow @snapinfraction on Facebook" /></a>
+          </li>
+          <li>
+            <a href="https://instagram.com/snapinfraction" class="instagram-follow-button">
+              <img src="/instagram-icon.png" />
+            </a>
+          </li>
+          <!-- li>
+            <iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fsnapinfraction&color=black_white&size=50" style="width: 50px; height: 50px;">
+            </iframe> 
+          </li -->
+        </ul>
+        <!-- script src="js/scripts.js">
+        </script -->
+      </div>
+      <div id="slide6">
+      </div>
+      <footer class="content">&copy; 2017 Snap Infraction
+      </footer>
+    </div>
+  </body>
+</html>
