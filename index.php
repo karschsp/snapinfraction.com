efc3e520aa798062082fcfb228653d5c1cfa7e08
Steve Karsch <steve@stevekarsch.com>
2017-04-01 22:46:26 -0400

    Add metatags and store icons.

--- index.php
+++ index.php
@@ -4,8 +4,46 @@
     <meta charset="utf-8">
     <title>Snap Infraction - Chin Music
     </title>
-    <meta name="description" content="Snap Infraction - Chin Music">
+    <meta name="description" content="Independent rock n roll from Philadephia, PA. Debut EP &#39;Chin Music&#39; now available at online retailers everywhere.">
+    <meta name="keywords" content="music, powerpop, rock n roll, alternative, rock, philadelphia, steve karsch, tony iannuzzi, guitar bass drums, debut">    
     <meta name="author" content="Snap Infraction">
+    <meta name="pub_date" content="April 01, 2017"/>
+
+    <meta property="og:site_name" content="Snap Infraction"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="Snap Infraction - Chin Music"/>
+    <meta property="og:url" content="http://snapinfraction.com/"/>
+    <meta property="og:description" content="Independent rock n roll from Philadephia, PA. Debut EP &#39;Chin Music&#39; now available at online retailers everywhere."/>
+    <meta property="og:updated_time" content="2017-04-01T09:00:02-04:00"/>
+    <meta property="og:image" content="http://www.snapinfraction.com/img/snap_chin_music_cover.jpg"/>
+    <meta name="twitter:card" content="summary"/>
+    <meta name="twitter:site" content="@snapinfraction"/>
+    <meta name="twitter:site:id" content="55717504"/>
+    <meta name="twitter:creator" content="@snapinfraction"/>
+    <meta name="twitter:creator:id" content="55717504"/>
+
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/favicons/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/favicons/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/favicons/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/favicons/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/img/favicons/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/favicons/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/img/favicons/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/favicons/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="/img/favicons/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="/img/favicons/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="/img/favicons/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/img/favicons/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="/img/favicons/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="Snap Infraction"/>
+    <meta name="msapplication-TileColor" content="#000" />
+    <meta name="msapplication-TileImage" content="/img/favicons/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="/img/favicons/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="/img/favicons/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="/img/favicons/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="/img/favicons/mstile-310x310.png" />
+    
+    
     <link rel="stylesheet" href="/css/styles.css?v=1.0">
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css'>
     
@@ -53,21 +91,20 @@
         <h5>Buy:
         </h5>
         <ul class="links" id="buy">
-          <li>
-            <a href="">iTunes
+          <li class="store-icon">
+            <a class="itunes-icon" title="iTunes" href="">iTunes
             </a>
           </li>
-          <li>
-            <a href="">Amazon
+          <li class="store-icon">
+            <a class="amazon-icon" title="Amazon" href="">Amazon
             </a>
           </li>
-          <li>
-            <a href="">CDBaby
+          <li class="store-icon">
+            <a class="googleplay-icon" title="Google Play" href="">Google Play
             </a>
           </li>
-          <li>
-            <a href="">Google Music
-            </a>
+          <li class="store-icon">
+            <a class="spotify-icon" title="Spotify" href="">Spotify</a>
           </li>
         </ul>
       </div>
