be523fe87537a714c57521b60526436cd5b8379c
Steve Karsch <steve@stevekarsch.com>
2017-04-28 23:58:10 -0400

    Add header and footer includes.

--- beta.php
+++ beta.php
@@ -1,58 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Snap Infraction - Chin Music
-    </title>
-    <meta name="description" content="Independent alternative rock n roll from Philadephia, PA. Debut EP &#39;Chin Music&#39; now available at online retailers everywhere.">
-    <meta name="keywords" content="snap infraction, music, powerpop, rock n roll, alternative, rock, philadelphia, steve karsch, tony iannuzzi, guitar bass drums, debut, EP, chin music">    
-    <meta name="author" content="Snap Infraction">
-    <meta name="pub_date" content="April 01, 2017"/>
-
-    <meta property="og:site_name" content="Snap Infraction"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:title" content="Snap Infraction - Chin Music"/>
-    <meta property="og:url" content="http://snapinfraction.com/"/>
-    <meta property="og:description" content="Snap Infraction - independent rock n roll from Philadephia, PA. Debut EP &#39;Chin Music&#39; now available at online retailers everywhere."/>
-    <meta property="og:updated_time" content="2017-04-01T09:00:02-04:00"/>
-    <meta property="og:image" content="http://www.snapinfraction.com/img/snap_chin_music_cover.jpg"/>
-    <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:site" content="@snapinfraction"/>
-    <meta name="twitter:site:id" content="55717504"/>
-    <meta name="twitter:creator" content="@snapinfraction"/>
-    <meta name="twitter:creator:id" content="55717504"/>
-
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/favicons/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/favicons/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/favicons/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/favicons/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/img/favicons/apple-touch-icon-60x60.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/favicons/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/img/favicons/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/favicons/apple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-196x196.png" sizes="196x196" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-128.png" sizes="128x128" />
-    <meta name="application-name" content="Snap Infraction"/>
-    <meta name="msapplication-TileColor" content="#000" />
-    <meta name="msapplication-TileImage" content="/img/favicons/mstile-144x144.png" />
-    <meta name="msapplication-square70x70logo" content="/img/favicons/mstile-70x70.png" />
-    <meta name="msapplication-square150x150logo" content="/img/favicons/mstile-150x150.png" />
-    <meta name="msapplication-wide310x150logo" content="/img/favicons/mstile-310x150.png" />
-    <meta name="msapplication-square310x310logo" content="/img/favicons/mstile-310x310.png" />
-    
-    
-    <link rel="stylesheet" href="/css/styles.css?v=1.0">
-    <!--link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css' -->
-    
-    <!--[if lt IE 9]>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-<![endif]-->
-  </head>
-  <body>
-    <div id="container">
+<?php
+$page_title = "Chin Music";
+$pub_date = "2017-04-01T09:00:02-04:00";
+require_once('/includes/header.php');
+?>
       <div class="content">
         <h1>Snap Infraction
         </h1>
@@ -209,18 +159,4 @@
       </div>
       <div id="slide6">
       </div>
-      <footer class="content">&copy; 2017 Snap Infraction
-      </footer>
-    </div>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-741455-8', 'auto');
-  ga('send', 'pageview');
-
-</script>
-  </body>
-</html>
+<?php require_once('/includes/footer.php'); ?>
