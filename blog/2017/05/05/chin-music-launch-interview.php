0e56301cca986137d596982d145297b74b19536c
Steve Karsch <steve@stevekarsch.com>
2017-05-03 21:08:23 -0400

    Clean up formatting on the interview.

--- blog/2017/05/05/chin-music-launch-interview.php
+++ blog/2017/05/05/chin-music-launch-interview.php
@@ -4,116 +4,118 @@ $pub_date = "2017-05-05T07:13:31-04:00";
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 ?>
 <div class="content">
+<h3 class="title"><?php print $page_title; ?></h3>
+<p class="pubDate">2017.05.05</p>
 <p>To celebrate the release of "Chin Music," Steve and Tony sat down with old friend and former Rockpile magazine journalist, Rusty Pinkerton to talk about the history of the band, the making of "Chin Music" and more. Enjoy!</p>
 <hr />
-
-<p>I: OK, let's start at the beginning.
-S: Well, which beginning, because we could go way back.
-T: Right, back before we remember.
+<img class="right" src="/img/opt/steve_tony_promo.jpg" alt="Steve and Tony at Bar 9 NYC" />
+<p><span class="question"><strong>Rusty:</strong> OK, let's start at the beginning.</span>
+<span class="answer"><strong>Steve:</strong> Well, which beginning, because we could go way back.<br />
+<strong>Tony:</strong> Right, back before we remember.</span>
 </p>
 <p>
-I: Alright, the beginning of this band, Snap Infraction.
-T: It's not really even a band.
-S: Right, it's like a fake band. Like most of our other projects.  Like, we don't even have a MAIN project, we just have side projects.
-T: That's a good band name; The Side Projects.
-S: Right.
+<p><span class="question"><strong>Rusty:</strong>Alright, the beginning of this band, Snap Infraction.</span>
+<span class="answer"><strong>Tony:</strong> It's not really even a band.<br />
+<strong>Steve: </strong> Right, it's like a fake band. Like most of our other projects.  Like, we don't even have a MAIN project, we just have side projects.<br />
+<strong>Tony: </strong> That's a good band name; The Side Projects.<br />
+<strong>Steve: </strong> Right.
 </p>
 <p>
-I: So talk about the beginning of this side project.
-S: Well, Tony and I had left our previous band, Great Jones and I'd always wanted to do a solo album a la McCartney/McCartney and the Jason Falkner records: write, play, engineer everything myself. The only problem was, I could write and sorta play, but drumming and engineering was definitely way out of my realm of expertise. I'd just moved into a new house and had one of Tony's drumkits there and would demo stuff and send it over to him and he would be like "Yeah, that's cool" or "yeah, that sucks" or "yeah, you're drunk."
-T: Or all of the above.
-S: Exactly. 
+<span class="question"><strong>Rusty: </strong>So talk about the beginning of this side project.</span>
+<strong>Steve: </strong>Well, Tony and I had left our previous band, Great Jones and I'd always wanted to do a solo album a la McCartney/McCartney and the Jason Falkner records: write, play, engineer everything myself. The only problem was, I could write and sorta play, but drumming and engineering was definitely way out of my realm of expertise. I'd just moved into a new house and had one of Tony's drumkits there and would demo stuff and send it over to him and he would be like "Yeah, that's cool" or "yeah, that sucks" or "yeah, you're drunk."<br />
+<strong>Tony: </strong>Or all of the above.<br />
+<strong>Steve: </strong>Exactly. 
 </p>
 <p>
-I: So, when was this exactly?
-T: We really don't work on "exact" terms.
-S: Yeah, we don't know exactly. I moved into that house in...2004, I think?
-T: Sounds about right.
-S: But the first few songs I demo'd didn't make this project. Well, yet. They may at some point in the future.
-T: Yeah, and then there was Nostril.
-S: Oh RIGHT. Yeah, so even before THIS side project got off the ground, we started yet another side project called Nostril. And we actually recorded basic tracks, not just demos, for that project at our friend Leo's studio. And then we just left them.
-T: Probably because we got side-tracked with something else, Clone Radio or something.
+<span class="question"><strong>Rusty: </strong>So, when was this exactly?</span>
+<strong>Tony: </strong>We really don't work on "exact" terms.<br />
+<strong>Steve: </strong>Yeah, we don't know exactly. I moved into that house in...2004, I think?<br />
+<strong>Tony: </strong>Sounds about right.<br />
+<strong>Steve: </strong>But the first few songs I demo'd didn't make this project. Well, yet. They may at some point in the future.<br />
+<strong>Tony: </strong>Yeah, and then there was Nostril.<br />
+<strong>Steve: </strong>Oh RIGHT. Yeah, so even before THIS side project got off the ground, we started yet another side project called Nostril. And we actually recorded basic tracks, not just demos, for that project at our friend Leo's studio. And then we just left them.<br />
+<strong>Tony: </strong>Probably because we got side-tracked with something else, Clone Radio or something.
 </p>
 <p>
-I: Wait, so you've mentioned two other bands starting SINCE the start of Snap Infraction...
-S: Yeah, and we hadn't yet recorded a note or even written a Snap song at this point.
+<span class="question"><strong>Rusty: </strong>Wait, so you've mentioned two other bands starting SINCE the start of Snap Infraction...</span>
+<strong>Steve: </strong>Yeah, and we hadn't yet recorded a note or even written a Snap song at this point.<br />
 </p>
 <p>
-I: So, when did THAT happen?
-S: I think around 2005/2006 I started toying around with "You'll Never Know." I was, well, I think we both were, listening to a lot of Elliott Smith around that time.
-T: Yeah, "Basement On A Hill" and "XO" mostly...
-S: ...so I think I was stealing, or trying to steal, from that. So, I demo'd that and we both liked it and attempted to record it.
-T: For this first time in probably 2007.
-S: And then again in 2009.
-T: And again...
-S: Right...wash, rinse, repeat...
+<span class="question"><strong>Rusty: </strong>So, when did THAT happen?</span>
+<strong>Steve: </strong>I think around 2005/2006 I started toying around with "You'll Never Know." I was, well, I think we both were, listening to a lot of Elliott Smith around that time.<br />
+<strong>Tony: </strong>Yeah, "Basement On A Hill" and "XO" mostly...<br />
+<strong>Steve: </strong>...so I think I was stealing, or trying to steal, from that. So, I demo'd that and we both liked it and attempted to record it.<br />
+<strong>Tony: </strong>For this first time in probably 2007.<br />
+<strong>Steve: </strong>And then again in 2009.<br />
+<strong>Tony: </strong>And again...<br />
+<strong>Steve: </strong>Right...wash, rinse, repeat...
 </p>
 <p>
-I: And HOW many times has that song been recorded?
-T: We've completely lost track.
-S: Yeah, it's a mess. As a matter of fact, we had the final masters, and we were about to give it the final stamp of approval, like "OK, print it, ship it, deliver it, whatever" when I remembered we had cut a tiny additional piano and vocal part, the part right at the end
-T: so it was really one of those "stop the presses" moments
-S: Right. So I had to dig through our archives of all our various takes of that song and find the audio files of the parts I was looking for and add that to the mix.
-T: and send it off to cliff for the final FINAL FINAL final master of that song.
-S: and I still don't think it's right. But yeah, that's 1 song in 10 years.
+<span class="question"><strong>Rusty: </strong>And HOW many times has that song been recorded?</span>
+<strong>Tony: </strong>We've completely lost track.<br />
+<strong>Steve: </strong>Yeah, it's a mess. As a matter of fact, we had the final masters, and we were about to give it the final stamp of approval, like "OK, print it, ship it, deliver it, whatever" when I remembered we had cut a tiny additional piano and vocal part, the part right at the end<br />
+<strong>Tony: </strong>so it was really one of those "stop the presses" moments<br />
+<strong>Steve: </strong>Right. So I had to dig through our archives of all our various takes of that song and find the audio files of the parts I was looking for and add that to the mix.<br />
+<strong>Tony: </strong>and send it off to cliff for the final FINAL FINAL final master of that song.<br />
+<strong>Steve: </strong>and I still don't think it's right. But yeah, that's 1 song in 10 years.<br />
 </p>
 <p>
-I: Surely not every song was that tortuous.
-T: Not EVERY song.
-S: I'd say about half and half. "Try To See It My Way" was written and recorded in one night.
-T: Yeah, and mixed the next day.
-S: I had just seen Sloan a few weeks earlier, who I'm a huge fan of, and was just reminded of how awesome and sometimes straightforward their songs could be, so I wanted to do something along those lines. I think we just jammed out the basic song structure 5 or 6 times, cut a couple takes, overdubbed bass, added vocals and percussion and that was it.
-T: We definitely didn't over think that one.
+<span class="question"><strong>Rusty: </strong>Surely not every song was that tortuous.</span>
+<strong>Tony: </strong>Not EVERY song.<br />
+<strong>Steve: </strong>I'd say about half and half. "Try To See It My Way" was written and recorded in one night.<br />
+<strong>Tony: </strong>Yeah, and mixed the next day.<br />
+<strong>Steve: </strong>I had just seen Sloan a few weeks earlier, who I'm a huge fan of, and was just reminded of how awesome and sometimes straightforward their songs could be, so I wanted to do something along those lines. I think we just jammed out the basic song structure 5 or 6 times, cut a couple takes, overdubbed bass, added vocals and percussion and that was it.<br />
+<strong>Tony: </strong>We definitely didn't over think that one.<br />
 </p>
 <p>
-I: Any more like that?
-S: Actually, I take back my half-and-half comment from before. The rest of them came together fairly quickly. Well, quickly in our timeframe, which could mean years.
+<span class="question"><strong>Rusty: </strong>Any more like that?</span>
+<strong>Steve: </strong>Actually, I take back my half-and-half comment from before. The rest of them came together fairly quickly. Well, quickly in our timeframe, which could mean years.<br />
 </p>
 <p>
-I: Like?
-S: "Window Seat" was one that I'd actually forgotten I had written! I was listening to an old cassette of stuff I had demo'd in like 1999 or somewhere around then, and I heard that opening chord progression, just me strumming it, no vocals or melody or anything, and I liked it. So I just finished writing it, added the little bridge part, and that was it.
-T: So, from the first version of that song in 1999, to now, is almost 20 years!
-S: Yeah. 10 of which that song was completely lost to time.
+<span class="question"><strong>Rusty: </strong>Like?</span>
+<strong>Steve: </strong>"Window Seat" was one that I'd actually forgotten I had written! I was listening to an old cassette of stuff I had demo'd in like 1999 or somewhere around then, and I heard that opening chord progression, just me strumming it, no vocals or melody or anything, and I liked it. So I just finished writing it, added the little bridge part, and that was it.<br />
+<strong>Tony: </strong>So, from the first version of that song in 1999, to now, is almost 20 years!<br />
+<strong>Steve: </strong>Yeah. 10 of which that song was completely lost to time.<br />
 </p>
 <p>
-I: Tell me about the recording process. Since there's no band, there's not really a chance to record live in the studio, not that most bands really do that anymore anyway...
-S: Exactly. And we're very rarely in the same place at the same time anyway, unless we're playing gigs...
+<span class="question"><strong>Rusty: </strong>Tell me about the recording process. Since there's no band, there's not really a chance to record live in the studio, not that most bands really do that anymore anyway...</span>
+<strong>Steve: </strong>Exactly. And we're very rarely in the same place at the same time anyway, unless we're playing gigs...<br />
 </p>
 <p>
-I: Snap Infraction gigs?
-S: No, no...gigs with other bands. Cover bands or whatever.
+<span class="question"><strong>Rusty: </strong>Snap Infraction gigs?</span>
+<strong>Steve: </strong>No, no...gigs with other bands. Cover bands or whatever.
 </p>
 <p>
-I: Oh, OK.
-S: So the process would usually be, I'd cut a demo, with programmed drums and everything so Tony had an idea of what I was looking for on the track, I'd send a bounce to him with the drums taken out, and he'd cut his drums to that, send it back to me, and then I'd add bass, guitars, percussion, keyboards, etc.
-T: And then he'd come over to my studio to cut vocals.
-S: And then we'd go back and forth over mixes for an eternity, until we finally gave up.
+<span class="question"><strong>Rusty: </strong>Oh, OK.</span>
+<strong>Steve: </strong>So the process would usually be, I'd cut a demo, with programmed drums and everything so Tony had an idea of what I was looking for on the track, I'd send a bounce to him with the drums taken out, and he'd cut his drums to that, send it back to me, and then I'd add bass, guitars, percussion, keyboards, etc.<br />
+<strong>Tony: </strong>And then he'd come over to my studio to cut vocals.<br />
+<strong>Steve: </strong>And then we'd go back and forth over mixes for an eternity, until we finally gave up.<br />
 </p>
 <p>
-I: So, that's where Cliff Hillis came in?
-T: Yeah, we just got tired of having the same arguments over and over.
+<span class="question"><strong>Rusty: </strong>So, that's where Cliff Hillis came in?</span>
+<strong>Tony: </strong>Yeah, we just got tired of having the same arguments over and over.<br />
 </p>
 <p>
-I: How did you know him?
-S: I've known OF him for years. I saw his band Starbelly back at the Grape Street in Philly in 1998-1999 and sort of followed his solo career since then. And then I had the opportunity to play with him on a couple gigs for Tommy Conwell, so I got to know him and kept him in the back of my mind as someone we could possible get to work on our stuff.
-T: We were originally just going to get him to master everything, but once we hit our wall with the mixing, we thought "Let's just hire Cliff to do it. He'll do a way better job than us anyway and in way less time." And we were right. For once.
-S: I think it actually worked out because I like both of the mixing approaches. I like Tony's approach on the songs that he did and I like Cliff's approach on his songs. It adds a little more variety to the sound, I'm not sure anyone else notices, but I do and I like it.
+<span class="question"><strong>Rusty: </strong>How did you know him?</span>
+<strong>Steve: </strong>I've known OF him for years. I saw his band Starbelly back at the Grape Street in Philly in 1998-1999 and sort of followed his solo career since then. And then I had the opportunity to play with him on a couple gigs for Tommy Conwell, so I got to know him and kept him in the back of my mind as someone we could possible get to work on our stuff.<br />
+<strong>Tony: </strong>We were originally just going to get him to master everything, but once we hit our wall with the mixing, we thought "Let's just hire Cliff to do it. He'll do a way better job than us anyway and in way less time." And we were right. For once.<br />
+<strong>Steve: </strong>I think it actually worked out because I like both of the mixing approaches. I like Tony's approach on the songs that he did and I like Cliff's approach on his songs. It adds a little more variety to the sound, I'm not sure anyone else notices, but I do and I like it.<br />
 </p>
 <p>
-I: So, as you've mentioned, there's only two of you, there's not really a "band" per se. Any plans to put one together to play live?
-T: Honestly, at this point, no. I mean, we love playing live, and we know more than enough people to field a great live band, but we're almost too old to do the live original thing. I don't know, anything's possible.
+<span class="question"><strong>Rusty: </strong>So, as you've mentioned, there's only two of you, there's not really a "band" per se. Any plans to put one together to play live?</span>
+<strong>Tony: </strong>Honestly, at this point, no. I mean, we love playing live, and we know more than enough people to field a great live band, but we're almost too old to do the live original thing. I don't know, anything's possible.<br />
 </p>
 <p>
-I: Will we need to wait until 2027 to hear any new Snap Infraction material?
-S: We certainly hope not. The original plan for "Chin Music" was for it to be a full-length album, and we cut all the basic tracks for the other half of it. So we have at least 5 more songs ready to build on, but we're already planning the next EP.
-T: We have drums for 4 or 5 songs already, with maybe 2 more that need to be cut, so once Steve and his massive ego finish their guitar parts, we'll be ready to go.
-S: Yeah, I tend to get carried away when cutting guitars, so I'm trying to make a conscious effort to strip things back for this next set of songs. I want that "Rubber Soul" feel and sound to it. Easier said than done, of course...
-T: So, hopefully you won't have to wait until 2027, but we're not promising anything. 2022?
-S: Yeah, 2022 sounds about right.
+<span class="question"><strong>Rusty: </strong>Will we need to wait until 2027 to hear any new Snap Infraction material?</span>
+<strong>Steve: </strong>We certainly hope not. The original plan for "Chin Music" was for it to be a full-length album, and we cut all the basic tracks for the other half of it. So we have at least 5 more songs ready to build on, but we're already planning the next EP.<br />
+<strong>Tony: </strong>We have drums for 4 or 5 songs already, with maybe 2 more that need to be cut, so once Steve and his massive ego finish their guitar parts, we'll be ready to go.<br />
+<strong>Steve: </strong>Yeah, I tend to get carried away when cutting guitars, so I'm trying to make a conscious effort to strip things back for this next set of songs. I want that "Rubber Soul" feel and sound to it. Easier said than done, of course...<br />
+<strong>Tony: </strong> So, hopefully you won't have to wait until 2027, but we're not promising anything. 2022?<br />
+<strong>Steve: </strong>Yeah, 2022 sounds about right.<br />
 </p>
 <p>
-I: Last question, what is the meaning behind the name "Chin Music?"
-S: Oh, nothing really. We're both big sports fans, hence the band name Snap Infraction, a football term, so I guess Tony was watching baseball one night and the pitcher threw one high and inside, and the announcer said something to effect of "And he gave him a little chin music," or something like that. And that was that. That was the name of the album. Before it even existed.
+<span class="question"><strong>Rusty: </strong>Last question, what is the meaning behind the name "Chin Music?"</span>
+<strong>Steve: </strong>Oh, nothing really. We're both big sports fans, hence the band name Snap Infraction, a football term, so I guess Tony was watching baseball one night and the pitcher threw one high and inside, and the announcer said something to effect of "And he gave him a little chin music," or something like that. And that was that. That was the name of the album. Before it even existed.<br />
 </p>
 </div>
 <?php
