f84afbc474c9f5767c543bb7c4f16b42fc4ae7a4
Steve Karsch <steve@stevekarsch.com>
2017-05-05 07:32:34 -0400

    Rename beta to index for launch.

--- index.php
+++ index.php
@@ -1,91 +1,107 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Snap Infraction - Chin Music
-    </title>
-    <meta name="description" content="Independent alternative rock n roll from Philadephia, PA. Debut EP &#39;Chin Music&#39; now available at online retailers everywhere.">
-    <meta name="keywords" content="snap infraction, music, powerpop, rock n roll, alternative, rock, philadelphia, steve karsch, tony iannuzzi, guitar bass drums, debut, EP, chin music">    
-    <meta name="author" content="Snap Infraction">
-    <meta name="pub_date" content="April 01, 2017"/>
-
-    <meta property="og:site_name" content="Snap Infraction"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:title" content="Snap Infraction - Chin Music"/>
-    <meta property="og:url" content="http://snapinfraction.com/"/>
-    <meta property="og:description" content="Snap Infraction - independent rock n roll from Philadephia, PA. Debut EP &#39;Chin Music&#39; now available at online retailers everywhere."/>
-    <meta property="og:updated_time" content="2017-04-01T09:00:02-04:00"/>
-    <meta property="og:image" content="http://www.snapinfraction.com/img/snap_chin_music_cover.jpg"/>
-    <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:site" content="@snapinfraction"/>
-    <meta name="twitter:site:id" content="55717504"/>
-    <meta name="twitter:creator" content="@snapinfraction"/>
-    <meta name="twitter:creator:id" content="55717504"/>
-
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/img/favicons/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/favicons/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/favicons/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/favicons/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="/img/favicons/apple-touch-icon-60x60.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/img/favicons/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/img/favicons/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/img/favicons/apple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-196x196.png" sizes="196x196" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="/img/favicons/favicon-128.png" sizes="128x128" />
-    <meta name="application-name" content="Snap Infraction"/>
-    <meta name="msapplication-TileColor" content="#000" />
-    <meta name="msapplication-TileImage" content="/img/favicons/mstile-144x144.png" />
-    <meta name="msapplication-square70x70logo" content="/img/favicons/mstile-70x70.png" />
-    <meta name="msapplication-square150x150logo" content="/img/favicons/mstile-150x150.png" />
-    <meta name="msapplication-wide310x150logo" content="/img/favicons/mstile-310x150.png" />
-    <meta name="msapplication-square310x310logo" content="/img/favicons/mstile-310x310.png" />
-    
-    
-    <link rel="stylesheet" href="/css/styles.css?v=1.0">
-    <style type="text/css">
-      #container {
-        background: none;
-        padding-top: 200px;
-      }
-
-      h3 {
-        border: 3px solid #fff;
-        padding: 16px;
-        display: block;
-        text-align: center;
-        margin: auto;
-        width: 50%;
-      
-      }
-
-      @media screen and (min-width: 320px) {
-        h3 {width: 75%;}
-      }
-
-    @media only screen and (max-width: 920px) and (min-width: 0px) {}
-      #follow li a img {
-        width: 75px;
-      }
-    }      
-    </style>
-    <!--link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css' -->
-    
-    <!--[if lt IE 9]>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-<![endif]-->
-  </head>
-  <body>
-    <div id="container">
+<?php
+$page_title = "Chin Music";
+$pub_date = "2017-04-01T09:00:02-04:00";
+require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+?>
+      <div id="slide1">
+      </div>
       <div class="content">
-        <h1>Snap Infraction
-        </h1>
-        <h2>Chin Music
-        </h2>
-        <h3 style="margin-bottom: 64px;">Coming 5.5.17</h3>
-              <ul id="follow" stye="margin-top: 32px;" class="links">
+        <a name="music">
+        </a>
+        <h3>Music
+        </h3>
+        <h4>Chin Music
+        </h4>
+        <iframe src="https://embed.spotify.com/?uri=spotify%3Aalbum%3A1SByLxhb6krGFjvCMaWOKA" width="100%" height="380" frameborder="0" allowtransparency="true"></iframe>
+        <h5>Buy:
+        </h5>
+        <ul class="links" id="buy">
+          <li class="store-icon">
+            <a class="itunes-icon" title="iTunes" href="https://itunes.apple.com/us/album/chin-music-ep/id1228275556">iTunes
+            </a>
+          </li>
+          <li class="store-icon">
+            <a class="amazon-icon" title="Amazon" href="https://www.amazon.com/Chin-Music-Snap-Infraction/dp/B06ZYVDD8G/ref=sr_1_1?ie=UTF8&qid=1493778788&sr=8-1&keywords=snap+infraction">Amazon
+            </a>
+          </li>
+          <li class="store-icon">
+            <a class="googleplay-icon" title="Google Play" href="https://play.google.com/store/music/album/Snap_Infraction_Chin_Music?id=Bkejf33cch46hemrets7tizrafm">Google Play
+            </a>
+          </li>
+          <li class="store-icon">
+            <a class="spotify-icon" title="Spotify" href="https://open.spotify.com/album/1SByLxhb6krGFjvCMaWOKA">Spotify</a>
+          </li>
+        </ul>
+      </div>
+      <div id="slide2">
+      </div>
+      <div class="content">
+        <h4>Credits
+        </h4>
+        <p>
+          <strong>Snap Infraction is:
+          </strong>
+          <br />
+          <strong>Tony Iannuzzi
+          </strong> - Drums
+          <br />
+          <strong>Steve Karsch
+          </strong> - Everything but drums
+        </p>
+        <p>Produced by Snap Infraction
+        </p>
+        <P>Engineered and mixed by Tony Iannuzzi
+          <br /> "The Historian" and "Window Seat" mixed by 
+          <a href="http://www.cliffhillis.com">Cliff Hillis
+          </a>
+          </p>
+        <p>Recorded in Cinnaminson, NJ, Riverton, NJ, Aston, PA, Denver, CO.
+        </p>
+        <p>Mastered by 
+          <a href="http://www.cliffhillis.com">Cliff Hillis
+          </a> at The Hacienda in Phoenixville, PA.
+        </p>
+        <p>Cover photo by Steve Karsch
+        </p>
+        <p>Songs written by Steve Karsch (&copy; 2017 157 Music 
+          <a href="http://www.bmi.com">BMI
+          </a>)
+        </p>
+        <p>Thanks for listening.
+        </p>
+      </div>
+      <div id="slide3">
+      </div>
+      <div class="content">
+        <a name="bio">
+        </a>
+        <h3>Bio
+        </h3>
+        <p>
+          <strong>Chin Music
+          </strong>, the debut EP from Philadelphia-area duo Snap Infraction, is a tight collection of 5 original hook-heavy songs drawing on the band's wide array of influences. Written over a span of ten years, and recorded mostly at home in the span of ten months, the EP is a straight-forward rock record with a nod toward the classics: Beatles, Stones and Zeppelin, without relying heavily on any particular influence.
+        </p>
+        <p>Songwriter and multi-instrumentalist Steve Karsch explains: "At the end of the day, what we really care about is the song. It may have a country flavor to it, a heavy blues rock vibe, or a sickenly-sweet pop melody; if it's a good song, if it's a song we both like, it's a song for Snap."
+        </p>
+        <p>After years of playing together in bands up and down the east coast, Steve and drummer/audio engineer Tony Iannuzzi grew tired of the same old band politics, and vowed to start a project on their own terms, they just didn't think it would take this long.
+        </p>
+        <p>"The plan for Snap Infraction has always been that there is no plan. We may never release another record, or we could start recording a new album tomorrow...or we could wait another decade. You never know."
+        </p>
+        <p>After writing and demoing up to 50 songs, the pair decided it was finally time to buckle down and introduce the world to Snap Infraction. Not willing to compromise on their vision, the duo decided to take on all aspects of production, from writing and performing to engineering and mixing. When it came time to put the finishing touches on the record, the pair called in power-pop legend Cliff Hillis to help with final mixes and mastering.
+        </p>
+        <p>"We've always been fans of the classics," Karsch continues. "And of the guitar-bass-drums combination. No frills, nothing fancy, just good, solid rock n roll."
+        </p>
+        <p>The result is Chin Music, a short trip through the past half-century of rock n roll with a look towards what may lie ahead.
+        </p>
+      </div>
+      <div id="slide5">
+      </div>
+      <div class="content">
+        <a name="follow">
+        </a>
+        <h3>Follow
+        </h3>
+        <ul id="follow" class="links">
           <li>
             <a href="https://twitter.com/snapinfraction"><img src="/img/opt/twitter-icon.png" alt="Follow @snapinfraction on Twitter" /></a>
           </li>
@@ -102,18 +118,9 @@
             </iframe> 
           </li -->
         </ul>
-      <footer class="content">&copy; 2017 Snap Infraction
-      </footer>        
-    </div>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-741455-8', 'auto');
-  ga('send', 'pageview');
-
-</script>
-  </body>
-</html>
+        <!-- script src="js/scripts.js">
+        </script -->
+      </div>
+      <div id="slide6">
+      </div>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'); ?>
